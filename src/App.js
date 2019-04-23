c9f4f68c94804b55e28fcbc1c1451e507dd29dc3
장학상 <janghaksang@gmail.com>
2019-04-23 13:32:19 +0900

    필터 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,219 @@
-import React, { Component } from 'react';
-import './App.css';
+import React,{ Component } from 'react';
+import uuidv4 from 'uuid/v4';
 
 import Table from './Table'
 
+import './App.css';
+
 class App extends Component {
+  constructor(props,context) {
+    super(props,context);
+    this.state = this.initialState();
+  }
+
+  initialState() {
+    return {
+      tableSchema:{
+        rowHeaderVisible:true,
+        columnHeaderVisible:true,
+      },
+      rowSchema:{
+        0:{style:{color:'#a0a0a0'}},
+        1:{style:{color:'red'}},
+      },
+      columnSchema:[
+        {id:'col1',name:'id',type:'string',sort:{use:true,onClick:this.onSort},filter:{use:true,onClick:this.onFilter},className:null,style:{width:200},options:{mandatory:true,editable:true,limit:50}},
+        {id:'col2',name:'offendit',type:'number',sort:{use:true,onClick:this.onSort},filter:{use:true,onClick:this.onFilter},className:null,style:{},options:{mandatory:true,editable:true}},
+        {id:'col3',name:'co',type:'string',sort:{use:true,onClick:this.onSort},filter:{use:true,onClick:this.onFilter},className:null,style:{},options:{mandatory:true,editable:true}},
+      ],
+      data:[
+        {_row_id:`App-0-${uuidv4()}`,col1:'foo',col2:0,col3:'a'},
+        {_row_id:`App-1-${uuidv4()}`,col1:'bar',col2:1,col3:'b'},
+        {_row_id:`App-2-${uuidv4()}`,col1:'baz',col2:2,col3:'c'}
+      ],
+      sorting:{},
+      filtering:[],
+    };
+  }
+
+  filterFeatures(type) {
+    switch(type) {
+      case 'string': return ['==','!=','in','!in'];
+      case 'number': return ['==','!=','>','<','>=','<='];
+      default: return ['=='];
+    }
+  }
+
+  renderFilterFeature(feature,index) {
+    return (
+      <option key={index} value={feature}>{feature}</option>
+    )
+  }
+
+  renderFilterValue(filter,index) {
+    let value = filter.value||(()=>{
+      switch(filter.type) {
+        case 'string': return '';
+        case 'number': return '';
+        default: return '';
+      }
+    })();
+    return (
+      <input
+        type={(()=>{
+          switch(filter.type) {
+            case 'string': return 'text';
+            case 'number': return 'number';
+            default: return 'text';
+          }
+        })()}
+        value={value}
+        style={{
+          width:filter.scrollWidth
+        }}
+        onChange={this.onChangeFilterValue(index)}
+        />
+    );
+  }
+
+  renderFiltering() {
+    let {filtering} = this.state;
+    return (
+      <div className={'Filtering'}>
+        {filtering.map((filter,index)=>{
+          let filterFeatures = this.filterFeatures(filter.type);
+          return (
+            <div
+              key={filter.type+index}
+              className={'FilteringItem'}
+              >
+              <span>{filter.name}</span>
+              <select value={filter.feature} onChange={this.onChangeFilterFeature(index)}>
+                {filterFeatures.map((feature,i)=>this.renderFilterFeature(feature,i))}
+              </select>
+              {this.renderFilterValue(filter,index)}
+              <button onClick={this.onRemoveFilterigItem(index)}>{'✕'}</button>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  filterData(data) {
+    let {filtering} = this.state;
+    filtering.forEach((filter,index)=>{
+      if(filter.value==null) return;
+      data = data.filter((row,index)=>{
+        switch(filter.feature) {
+          case '==': return row[filter.id]===filter.value;
+          case '!=': return row[filter.id]!==filter.value;
+          case 'in': return 0 <= row[filter.id].indexOf(filter.value);
+          case '!in': return !(0 <= row[filter.id].indexOf(filter.value));
+          case '>': return row[filter.id] > filter.value;
+          case '<': return row[filter.id] < filter.value;
+          case '>=': return row[filter.id] >= filter.value;
+          case '<=': return row[filter.id] <= filter.value;
+          default: return false;
+        }
+      });
+    })
+    return data;
+  }
+
   render() {
+    let {tableSchema,rowSchema,columnSchema,data} = this.state;
     return (
       <div className={'App'}>
+        {this.renderFiltering()}
         <Table
           style={{
             width:'100%',
           }}
-          tableSchema={{
-            rowHeaderVisible:false,
-            columnHeaderVisible:true,
-          }}
-          rowSchema={{
-            1:{name:'alpha',style:{width:65}},
-            3:{name:'gamma'},
-          }}
-          columnSchema={[
-            {id:'col1',name:'id',type:'string',sort:{use:true},filter:{use:true},mandatory:true,editable:true,className:null,style:{width:200},options:{limit:50}},
-            {id:'col2',name:'offendit',type:'text',sort:{use:true},filter:{use:true},mandatory:true,editable:true,className:null,style:{},options:{}},
-            {id:'col3',name:'co',type:'number',sort:{use:true},filter:{use:true},mandatory:true,editable:true,className:null,style:{},options:{}},
-          ]}
-          data={[
-            {col1: 'foo', col2: 0, col3: 'a'},
-            {col1: 'bar', col2: 1, col3: 'b'},
-            {col1: 'baz', col2: 2, col3: 'c'}
-          ]}
+          tableSchema={tableSchema}
+          rowSchema={rowSchema}
+          columnSchema={columnSchema}
+          data={this.filterData(data)}
           />
       </div>
     );
+  }
+
+  sortRows = (id,order)=> (a,b)=>{
+    if(a[id]==null&&b[id]==null) return 0;
+    if(a[id]==null) return order*-1;
+    if(b[id]==null) return order*1;
+    if(a[id]===b[id]) return 0;
+    return order*(a[id] < b[id]?-1:1);
+  }
+
+  onSort = (columnIndex,schema,props)=> (e)=>{
+    let {id} = schema;
+    this.setState(({data,sorting})=>{
+      if(id in sorting) {
+        sorting = {
+          ...sorting,
+          [id]:sorting[id]*-1
+        };
+      } else {
+        sorting = {
+          ...sorting,
+          [id]:-1
+        };
+      }
+      data.sort(this.sortRows(id,sorting[id]));
+      return {data,sorting};
+    });
+  }
+
+  onFilter = (columnIndex,schema,props)=> (e)=>{
+    let {id,name,type} = schema;
+    this.setState(({filtering})=>{
+      filtering = filtering.slice(0);
+      filtering.push({id,name,type,feature:'==',value:null,scrollWidth:50})
+      return {filtering};
+    })
+  }
+
+  onRemoveFilterigItem = (index)=> (e)=>{
+    this.setState(({filtering})=>{
+      filtering.splice(index,1);
+      return {filtering};
+    })
+  }
+
+  onChangeFilterFeature = (theIndex)=>(e)=>{
+    let feature = e.target.value;
+    this.setState(({filtering})=>{
+      let theItem = filtering[theIndex];
+      theItem = {
+        ...theItem,
+        feature,
+      };
+      filtering = filtering.map((item,index)=>index===theIndex?theItem:item);
+      return {filtering};
+    })
+  }
+
+  castValue(type,value) {
+    switch(type) {
+      case 'string': return value.toString();
+      case 'number': return parseInt(value,10);
+    }
+  }
+
+  onChangeFilterValue = (theIndex)=>(e)=>{
+    let {value,scrollWidth} = e.target;
+    this.setState(({filtering})=>{
+      let theItem = filtering[theIndex];
+      theItem = {
+        ...theItem,
+        value:this.castValue(theItem.type,value),
+        scrollWidth
+      };
+      filtering = filtering.map((item,index)=>index===theIndex?theItem:item);
+      return {filtering};
+    })
   }
 }
 
--- src/Table.js
+++ src/Table.js
@@ -30,18 +30,25 @@ function css(...args) {
 }
 
 export default class Table extends React.Component {
+  makeProps(events,columnIndex,schema,props) {
+    let es = {}
+    Object.keys(events).forEach(key=>{
+      es[key] = events[key](columnIndex,schema,props);
+    })
+    return es;
+  }
+
   renderHeaderColumnCellSortButton = (columnIndex,schema,props)=>{
+    if(props.renderHeaderColumnCellSortButton) return props.renderHeaderColumnCellSortButton(columnIndex,schema,props);
     let {name,sort} = schema||{};
     let {use,className,style,...events} = sort||{};
     name = name||columnToLetter(columnIndex+1);
-    style = {};
+    style = style||{};
     return (
       <button
         className={css('Sort',{'disabled':!use},className)}
         style={style}
-        {...Object.keys(events).map(key=>({
-          [key]:events[key](columnIndex,schema,props)
-        }))}
+        {...this.makeProps(events,columnIndex,schema,props)}
         >
         {name}
       </button>
@@ -49,17 +56,16 @@ export default class Table extends React.Component {
   }
 
   renderHeaderColumnCellFilterButton = (columnIndex,schema,props)=>{
+    if(props.renderHeaderColumnCellFilterButton) return props.renderHeaderColumnCellFilterButton(columnIndex,schema,props);
     let {filter} = schema||{};
     let {use,className,style,element,...events} = filter||{};
     if(!use) return null;
-    style = {};
+    style = style||{};
     return (
       <button
         className={css('Filter',className)}
         style={style}
-        {...Object.keys(events).map(key=>({
-          [key]:events[key](columnIndex,schema,props)
-        }))}
+        {...this.makeProps(events,columnIndex,schema,props)}
         >
         {'⑂'}
       </button>
@@ -67,9 +73,8 @@ export default class Table extends React.Component {
   }
 
   renderHeaderColumnCell = (columnIndex,schema,props)=>{
+    if(props.renderHeaderColumnCell) return props.renderHeaderColumnCell(columnIndex,schema,props);
     let {className,style} = schema||{};
-    let renderHeaderColumnCellSortButton = props.renderHeaderColumnCellSortButton || this.renderHeaderColumnCellSortButton;
-    let renderHeaderColumnCellFilterButton = props.renderHeaderColumnCellFilterButton || this.renderHeaderColumnCellFilterButton;
     style = style||{};
     return (
       <th
@@ -78,14 +83,15 @@ export default class Table extends React.Component {
         style={style}
         >
         <div>
-          {renderHeaderColumnCellSortButton(columnIndex,schema,props)}
-          {renderHeaderColumnCellFilterButton(columnIndex,schema,props)}
+          {this.renderHeaderColumnCellSortButton(columnIndex,schema,props)}
+          {this.renderHeaderColumnCellFilterButton(columnIndex,schema,props)}
         </div>
       </th>
     );
   }
 
   renderHeaderRowCell = (rowIndex,schema,props)=>{
+    if(props.renderHeaderRowCell) return props.renderHeaderRowCell(rowIndex,schema,props);
     let {name,className,style} = schema||{};
     name = name||rowIndex+1;
     style = style||{};
@@ -100,6 +106,7 @@ export default class Table extends React.Component {
   }
 
   renderCell = (rowIndex,columnIndex,cellData,rowData,columnId,schema,props)=>{
+    if(props.renderCell) return props.renderCell(rowIndex,columnIndex,cellData,rowData,columnId,schema,props);
     return (
       <td
         key={columnId}
@@ -111,59 +118,64 @@ export default class Table extends React.Component {
   }
 
   renderHeaderBlock = (props)=>{
+    if(props.renderHeaderBlock) return props.renderHeaderBlock(props);
     return (<th className={css('HeaderCell')}/>);
   }
 
   renderHeaderRow = (props)=>{
-    let {tableSchema:{rowHeaderVisible},rowSchema:{className,style},columnSchema} = props;
-    let renderHeaderBlock = props.renderHeaderBlock||this.renderHeaderBlock;
-    let renderHeaderColumnCell = props.renderHeaderColumnCell||this.renderHeaderColumnCell;
+    if(props.renderHeaderRow) return props.renderHeaderRow(props);
+    let {tableSchema:{rowHeaderVisible},rowSchema,columnSchema} = props;
+    let {className,style} = rowSchema[0]||{};
     return (
       <tr
         className={css('HeaderRow',className)}
         style={style}
         >
-        {rowHeaderVisible&&renderHeaderBlock(props)}
-        {columnSchema&&columnSchema.length&&columnSchema.map((schema,index)=>renderHeaderColumnCell(index,schema,props))}
+        {rowHeaderVisible&&this.renderHeaderBlock(props)}
+        {columnSchema&&columnSchema.length&&columnSchema.map((schema,index)=>this.renderHeaderColumnCell(index,schema,props))}
       </tr>
     );
   }
 
   renderRow = (rowIndex,rowData,props)=>{
+    if(props.renderRow) return props.renderRow(rowIndex,rowData,props);
     let {tableSchema:{rowHeaderVisible},rowSchema,columnSchema} = props;
-    let {className,style} = rowSchema;
-    let renderHeaderRowCell = props.renderHeaderRowCell||this.renderHeaderRowCell;
-    let renderCell = props.renderCell||this.renderCell;
+    let {className,style} = rowSchema[rowIndex+1]||{};
     return (
       <tr
         key={rowIndex}
         className={css('Row',className)}
         style={style}
         >
-        {rowHeaderVisible&&renderHeaderRowCell(rowIndex,rowSchema[rowIndex+1],props)}
-        {columnSchema&&columnSchema.length&&columnSchema.map((schema,columnIndex)=>renderCell(rowIndex,columnIndex,rowData[schema.id],rowData,schema.id,columnSchema,props))}
+        {rowHeaderVisible&&this.renderHeaderRowCell(rowIndex,rowSchema[rowIndex+1],props)}
+        {columnSchema&&columnSchema.length&&columnSchema.map((schema,columnIndex)=>this.renderCell(rowIndex,columnIndex,rowData[schema.id],rowData,schema.id,columnSchema,props))}
       </tr>
     )
   }
 
   renderHeader(props) {
+    if(props.renderHeader) return props.renderHeader(props);
     let {tableSchema:{columnHeaderVisible}} = props;
-    let renderHeaderRow = props.renderHeaderRow||this.renderHeaderRow;
     return columnHeaderVisible&&(
       <thead>
-        {renderHeaderRow(props)}
+        {this.renderHeaderRow(props)}
       </thead>
     );
   }
 
   renderBody(props) {
+    if(props.renderBody) return props.renderBody(props);
     let {data} = props;
-    let renderRow = props.renderRow||this.renderRow;
-    return data&&renderRow&&data.map((rowData,rowIndex)=>renderRow(rowIndex,rowData,props));
+    if(!data) return null;
+    return (
+      <tbody>
+        {data.map((rowData,rowIndex)=>this.renderRow(rowIndex,rowData,props))}
+      </tbody>
+    );
   }
 
   render() {
-    let {className,style} = this.props;
+    let {className,style,...props} = this.props;
     return (
       <table
         className={css('Table',className)}
@@ -171,10 +183,8 @@ export default class Table extends React.Component {
           ...style,
         }}
         >
-        {this.renderHeader(this.props)}
-        <tbody>
-          {this.renderBody(this.props)}
-        </tbody>
+        {this.renderHeader(props)}
+        {this.renderBody(props)}
       </table>
     );
   }
